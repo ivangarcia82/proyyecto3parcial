8bced28e74b5972c1f15c5c787131356d7ac8688
Ivan Garcia <ivangarcia@MacBook-Pro-de-Ivan.local>
2020-07-28 21:31:28 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -1,45 +1,44 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const cors = require('cors')
+ const express = require("express");
+ 
+ const path = require("path");
+ const cors = require('cors')
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+ const bodyParser = require("body-parser");
+ const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 
 
 
 const mongoose = require("mongoose");
+const app = express();
+
 const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+   .then(() => console.log('MongoDB Connected...'))
+   .catch(err => console.log(err));
 
-app.use(cors())
+ app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/product', require('./routes/product'));
+ app.use('/api/users', require('./routes/users'));
+ app.use('/api/product', require('./routes/product'));
 
 
 
-app.use('/uploads', express.static('uploads'));
+ app.use('/uploads', express.static('uploads'));
 
-if (process.env.NODE_ENV === "production") {
+ if (process.env.NODE_ENV === "production") {
 
-  app.use(express.static("client/build"));
+ app.use(express.static("client/build"));
 
-  // index.html for all page routes
-  app.get("*", (req, res) => {
+   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+   });
+ }
 
-const port = process.env.PORT || 5000
+ const port = process.env.PORT || 2040
+ app.listen(port)
 
-app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
-});
